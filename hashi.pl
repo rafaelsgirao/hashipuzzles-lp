00d2335e0ccdaad9c6ada99a0e20f163f50d1d0e
Rafael Girão <git@rafael.ovh>
2022-01-21 15:51:39 +0000

    Commit before humberto

--- hashi.pl
+++ hashi.pl
@@ -51,16 +51,50 @@ ilhas(N_L, [LinhaPuzzle | CPuz], [LinhaIlhas | CIlhas]) :-
 %2.3 - vizinhas/3
 %----------
 
-vizinhas(Ilhas, Ilha, Vizinhas) : 
+vizinhas([], _, []).
+%TODO: Eliminar ilhas que estão entre duas ilhas, 
+%atm está a marcá las como vizinhas
+
+%Caso de pertencerem ah mesma linha
+vizinhas([Eilha | RIlhas], Ilha, [IlhaVizinha | RVizinhas]) :- 
+  obter_linha_ilha(Ilha, Ilha_linha),
+  obter_linha_ilha(Eilha, Eilha_linha),
+  Ilha_linha = Eilha_linha,
+  IlhaVizinha = Eilha,
+  vizinhas(RIlhas, Ilha, RVizinhas).
+
+%Brainstorm:
+%Como ver se duas ilhas não são vizinhas, mas partilham a mesma coluna ou linha?
+%
+%
+%
+%
+%Caso de pertenceram ah mesma coluna
+vizinhas([Eilha | RIlhas], Ilha, [IlhaVizinha | RVizinhas]) :- 
+  obter_col_ilha(Ilha, Ilha_col),
+  obter_col_ilha(Eilha, Eilha_col),
+  Ilha_col = Eilha_col,
+  IlhaVizinha = Eilha,
+  vizinhas(RIlhas, Ilha, RVizinhas).
+
+%Caso em que nao sao vizinhas
+vizinhas([_ | RIlhas], Ilha, [_ | RVizinhas]) :-
+  vizinhas(RIlhas, Ilha, RVizinhas).
+
+
 %----------
 %Codigo Auxiliar
 %----------
 
-%Construtor da estrutura Ilha
+%Construtor e seletores da estrutura Ilha
 faz_ilha(N, N_Linha, N_Col, ilha(N, (N_Linha, N_Col))).
 
-obter_linha_ilha(ilha(N, (N_Linha, N_Col)), N_Linha).
+obter_linha_ilha(ilha(_, (N_Linha, _)), N_Linha).
 
-obter_linha_ilha(ilha(N, (N_Linha, N_Col)), N_Col).
+obter_col_ilha(ilha(_, (_, N_Col)), N_Col).
 
-obter_nr_pontes_ilha(ilha(N, (N_Linha, N_Col)), N).
+obter_nr_pontes_ilha(ilha(N, (_, _)), N).
+
+%----------
+
+%Devolve q
